25f76ee15664227176da8ab760bde8867a03a314
Akshaya-paila <83501159+Akshaya-paila@users.noreply.github.com>
2021-06-10 01:00:29 +0530

    Add files via upload

---
+++ project.ino
@@ -0,0 +1,142 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+String command;
+String data="";
+
+void callback(char* topic, byte* payload, unsigned int payloadLength);
+
+// CHANGE TO YOUR WIFI CREDENTIALS
+const char* ssid = "Akshaya";//your wifi ssid
+const char* password = "akki@1234";//your password
+
+// CHANGE TO YOUR DEVICE CREDENTIALS AS PER IN IBM BLUMIX
+#define ORG "unneo9"
+#define DEVICE_TYPE "ESP32"
+#define DEVICE_ID "12345"
+#define TOKEN "12345678" //  Authentication Token OF THE DEVICE
+
+//  PIN DECLARATIONS 
+
+//-------- Customise the above values --------
+const char publishTopic[] = "iot-2/evt/Data/fmt/json";
+char server[] = ORG ".messaging.internetofthings.ibmcloud.com";
+char topic[] = "iot-2/cmd/home/fmt/String";// 
+char authMethod[] = "use-token-auth";
+char token[] = TOKEN;
+char clientId[] = "d:" ORG ":" DEVICE_TYPE ":" DEVICE_ID;
+
+
+WiFiClient wifiClient;
+PubSubClient client(server, 1883, callback, wifiClient);
+
+int publishInterval = 5000; // 30 seconds
+long lastPublishMillis;
+void publishData();
+
+void setup() {
+  
+  Serial.begin(115200);
+  Serial.println();
+
+ 
+  wifiConnect();
+  mqttConnect();
+   delay(2000);
+Serial.println("oled test");
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
+    Serial.println("SSD1306 allocation failed");
+    for(;;);
+  }
+}
+
+void loop() {
+
+
+ if (millis() - lastPublishMillis > publishInterval)
+  {
+    publishData();
+    lastPublishMillis = millis();
+  }
+  
+  if (!client.loop()) {
+    mqttConnect();
+  }
+}
+
+void wifiConnect() {
+  Serial.print("Connecting to "); Serial.print(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.print("WiFi connected, IP address: "); 
+  Serial.println(WiFi.localIP());
+}
+
+void mqttConnect() {
+  if (!client.connected()) 
+  {
+    Serial.print("Reconnecting MQTT client to "); Serial.println(server);
+    while (!client.connect(clientId, authMethod, token)) {
+      Serial.print(".");
+      delay(500);
+    }
+    
+    initManagedDevice();
+    Serial.println();
+  }
+}
+
+void initManagedDevice() {
+  if (client.subscribe(topic)) {
+   
+    Serial.println("subscribe to cmd OK");
+  } else {
+    Serial.println("subscribe to cmd FAILED");
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int payloadLength) {
+  
+  Serial.print("callback invoked for topic: ");
+  Serial.println(topic);
+
+  for (int i = 0; i < payloadLength; i++) {
+    
+    command+= (char)payload[i];
+  }
+  
+
+  control_func();
+  command= "";
+}
+
+void control_func()
+{
+  Serial.println("You need to take " +command+ " medicine");
+  
+
+    display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 10);
+  // Display static text
+  display.print("You need to take " +command+ " medicine");
+ 
+  display.display(); 
+  
+}
+
+void publishData() 
+{
+
+}
